dc844aa287e9528a424f9176a5c3e8faf8dd3259
yangfeng <yangfeng19980715@gmail.com>
2022-10-10 23:36:59 +0800

    test

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 
 import com.google.android.material.snackbar.Snackbar;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.util.Log;
@@ -27,6 +28,25 @@ public class MainActivity extends AppCompatActivity {
 
     private AppBarConfiguration appBarConfiguration;
     private ActivityMainBinding binding;
+
+    // 第一个参数是启动活动时传入的请求码
+    // 第二个是返回数据时传入的处理结果
+    // 第三个是携带数据的intent
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        // 由于在一个活动中可能用startActivityForResult方法去启动很多不同的活动，每一个活动返回的数据都会回调到onActivityResult这个方法中
+        // 因此我们首先要做的就是通过检查requestCode的值来判断数据来源
+        super.onActivityResult(requestCode, resultCode, data);
+        switch (requestCode) {
+            case 1:
+                if (resultCode == RESULT_OK) {
+                    String returnedData = data.getStringExtra("data_return");
+                    Toast.makeText(MainActivity.this, returnedData, Toast.LENGTH_SHORT).show();
+                }
+                break;
+            default:
+        }
+    }
 
     private static final String TAG = "MainActivity";
     @Override
@@ -61,10 +81,15 @@ public class MainActivity extends AppCompatActivity {
                 // intent.setData(Uri.parse("tel:10086"));
                 // startActivity(intent);
 
-                String data = "Hello, yangfeng";
+                // String data = "Hello, yangfeng";  // 7.
+                // Intent intent = new Intent(MainActivity.this, ThirdActivity.class);
+                // intent.putExtra("extra_data", data);  // extra_data是键，data是值
+                // startActivity(intent);
+
                 Intent intent = new Intent(MainActivity.this, ThirdActivity.class);
-                intent.putExtra("extra_data", data);  // extra_data是键，data是值
-                startActivity(intent);
+                startActivityForResult(intent, 1);  // 第二个参数是请求码(只需要是个唯一值就可以了)，用于在之后的回调中判断数据的来源
+
+
 
             }
         });
@@ -122,6 +147,7 @@ public class MainActivity extends AppCompatActivity {
         }
         return true;
     }
+
 
     @Override
     public boolean onSupportNavigateUp() {
--- app/src/main/java/com/example/myapplication/ThirdActivity.java
+++ app/src/main/java/com/example/myapplication/ThirdActivity.java
@@ -10,6 +10,13 @@ import android.widget.Toast;
 
 public class ThirdActivity extends AppCompatActivity {
 
+    @Override
+    public void onBackPressed() {
+        Intent intent = new Intent();
+        intent.putExtra("data_return", "Hello, yangfeng, backed");
+        setResult(RESULT_OK, intent);
+        finish();
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,13 +30,18 @@ public class ThirdActivity extends AppCompatActivity {
                     @Override
                     public void onClick(View v) {
                         // startActivity(new Intent(ThirdActivity.this, MainActivity.class));
-                        Intent intent = getIntent();  // getIntent()可以获取到启动这个activity的Intent
+                        // Intent intent = getIntent();  // getIntent()可以获取到启动这个activity的Intent
+                        // String data = intent.getStringExtra("extra_data");
+                        // Toast.makeText(ThirdActivity.this, data, Toast.LENGTH_SHORT).show();
 
-                        String data = intent.getStringExtra("extra_data");
-
-                        Toast.makeText(ThirdActivity.this, data, Toast.LENGTH_SHORT).show();
+                        // 通过按钮返回的话，使用这里的逻辑
+                        Intent intent = new Intent(); // 这个intent仅仅是用于传递数据，没有指定任何的意图
+                        intent.putExtra("data_return", "Hello, yangfeng, button clicked");  // 把数据存放在intent中
+                        setResult(RESULT_OK, intent);  // 这个函数专门向上一个活动返回数据,第一个参数返回处理结果
+                        finish();  // finish销毁当前活动
 
                     }
+
                 }
         );
 
